7f1987c7ce29ead7b0820ff768518bdf4f997d63
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-12 15:43:08 -0500

    add contact page

--- src/pages/Contact/contact.tsx
+++ src/pages/Contact/contact.tsx
@@ -1,5 +1,42 @@
+import style from "./contact.module.css";
+
+import img from "../../assets/mail.svg";
+import { Image } from "@chakra-ui/react";
+
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div
+      className={`center ${style["contact-page"]}`}
+      style={{ flexDirection: "column" }}
+    >
+      <Image src={img} alt="mail" boxSize="200px" />
+      <br />
+      <div>
+        You can reach me at{" "}
+        <a href="mailto:satshree.shr@gmail.com" style={{ fontStyle: "italic" }}>
+          satshree.shr@gmail.com
+        </a>
+      </div>
+      <br />
+      <div>
+        <a
+          href="https://github.com/satshree"
+          target="_blank"
+          style={{ marginRight: "0.5rem" }}
+        >
+          GitHub
+        </a>
+        |
+        <a
+          href="https://www.linkedin.com/in/satshree/"
+          target="_blank"
+          style={{ marginLeft: "0.5rem" }}
+        >
+          LinkedIn
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
