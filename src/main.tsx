26300ffbaf51439a5c4a03ab168c5f02802ac3b8
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-12 14:38:45 -0500

    apply chakra ui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import App from "./App.tsx";
 import "./index.css";
 
+const theme = extendTheme({
+  fonts: {
+    heading: `'Inter', Helvetica, sans-serif`,
+    body: `'Inter', Helvetica, sans-serif`,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
