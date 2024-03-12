d330c1f362217edf5e4b54bf189fcdaab51ed393
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-12 15:10:15 -0500

    home page

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
+
+import img from "../../assets/coding.svg";
+import style from "./home.module.css";
 
 function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div
+      className={`center ${style["home-page"]}`}
+      style={{ flexDirection: "column" }}
+    >
+      <Flex alignItems="center">
+        <Image src={img} alt="coding" boxSize="350px" />
+        <div className={style.details}>
+          <div className={style["main-title"]}>Satshree Shrestha</div>
+          <div className={style.title}>Full Stack Developer</div>
+          <br />
+          <div>Django, Spring Boot, React.js, Next.js</div>
+        </div>
+      </Flex>
+    </div>
+  );
 }
 
 export default Home;
