95487cbf668258385082f01b3b5728c07d4c269c
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-13 12:54:57 -0500

    setup reducers for redux

---
+++ src/app/reducers.ts
@@ -0,0 +1,32 @@
+import { GlobalState, ProjectData } from "../types";
+import { ActiveProjectAction, ActionTypes, ProjectListAction } from "./actions";
+
+const initialActiveProjectState: ProjectData = {
+  title: "",
+  description: "",
+  techStack: [],
+  images: [],
+};
+
+const initialProjectListState: ProjectData[] = [];
+
+const initialState: GlobalState = {
+  activeProject: initialActiveProjectState,
+  projectList: initialProjectListState,
+};
+
+const projectReducer = (
+  state: GlobalState = initialState,
+  action: ActiveProjectAction | ProjectListAction
+) => {
+  switch (action.type) {
+    case ActionTypes.ACTIVE_PROJECT:
+      return { ...state, activeProject: action.payload };
+    case ActionTypes.PROJECT_LIST:
+      return { ...state, projectList: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default projectReducer;
